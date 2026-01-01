4310ed9c5e0e505d96e8a05450e381e10f34b47c
smalllin0 <aslinqf@163.com>
2026-01-01 10:55:22 +0800

    优化组装响应状况字符串拼接，优化响应码转字符串编码格式

--- src/response/AsyncWebServerResponse.cc
+++ src/response/AsyncWebServerResponse.cc
@@ -48,21 +48,31 @@ std::string AsyncWebServerResponse::assembleHead(uint8_t version)
     }
 
     std::string out;
-    out.reserve(256);       //预先分配部分内存
+    out.reserve(512);       //预先分配部分内存
 
-    out = version == 0 ? "HTTP/1.0" : "HTTP/1.1";
-    out += ' ' + std::to_string(code_) + ' ' + responseCodeToString(code_) + "\r\n";
+    out += version == 0 ? "HTTP/1.0 " : "HTTP/1.1 ";
+    out += std::to_string(code_);
+    out.push_back(' ');
+    out += responseCodeToString(code_);
+    out += "\r\n";
 
     if (sendContentLength_) {
-        out += "Content-Length: " + std::to_string(contentLength_) + "\r\n";
+        out += "Content-Length: ";
+        out += std::to_string(contentLength_);
+        out += "\r\n";
     }
     
     if (!contentType_.empty()) {
-        out += "Content-Type: " + contentType_ + "\r\n";
+        out += "Content-Type: ";
+        out += contentType_;
+        out += "\r\n";
     }
 
     for(const auto& header : headers_) {
-        out += header->name() + ": " + header->value() + "\r\n";
+        out += header->name();
+        out += ": ";
+        out += header->value();
+        out += "\r\n";
     }
     headers_.free();
     out += "\r\n";
@@ -85,46 +95,46 @@ void AsyncWebServerResponse::respond(AsyncWebServerRequest* req) {
 const char* AsyncWebServerResponse::responseCodeToString(uint16_t code)
 {
     switch (code) {
-    case 100: return "Continue";
-    case 101: return "Switching Protocols";
-    case 200: return "OK";
-    case 201: return "Created";
-    case 202: return "Accepted";
-    case 203: return "Non-Authoritative Information";
-    case 204: return "No Content";
-    case 205: return "Reset Content";
-    case 206: return "Partial Content";
-    case 300: return "Multiple Choices";
-    case 301: return "Moved Permanently";
-    case 302: return "Found";
-    case 303: return "See Other";
-    case 304: return "Not Modified";
-    case 305: return "Use Proxy";
-    case 307: return "Temporary Redirect";
-    case 400: return "Bad Request";
-    case 401: return "Unauthorized";
-    case 402: return "Payment Required";
-    case 403: return "Forbidden";
-    case 404: return "Not Found";
-    case 405: return "Method Not Allowed";
-    case 406: return "Not Acceptable";
-    case 407: return "Proxy Authentication Required";
-    case 408: return "Request Time-out";
-    case 409: return "Conflict";
-    case 410: return "Gone";
-    case 411: return "Length Required";
-    case 412: return "Precondition Failed";
-    case 413: return "Request Entity Too Large";
-    case 414: return "Request-URI Too Large";
-    case 415: return "Unsupported Media Type";
-    case 416: return "Requested range not satisfiable";
-    case 417: return "Expectation Failed";
-    case 500: return "Internal Server Error";
-    case 501: return "Not Implemented";
-    case 502: return "Bad Gateway";
-    case 503: return "Service Unavailable";
-    case 504: return "Gateway Time-out";
-    case 505: return "HTTP Version not supported";
-    default:  return "Invalid code.";
+      case 100: return "Continue";
+      case 101: return "Switching Protocols";
+      case 200: return "OK";
+      case 201: return "Created";
+      case 202: return "Accepted";
+      case 203: return "Non-Authoritative Information";
+      case 204: return "No Content";
+      case 205: return "Reset Content";
+      case 206: return "Partial Content";
+      case 300: return "Multiple Choices";
+      case 301: return "Moved Permanently";
+      case 302: return "Found";
+      case 303: return "See Other";
+      case 304: return "Not Modified";
+      case 305: return "Use Proxy";
+      case 307: return "Temporary Redirect";
+      case 400: return "Bad Request";
+      case 401: return "Unauthorized";
+      case 402: return "Payment Required";
+      case 403: return "Forbidden";
+      case 404: return "Not Found";
+      case 405: return "Method Not Allowed";
+      case 406: return "Not Acceptable";
+      case 407: return "Proxy Authentication Required";
+      case 408: return "Request Time-out";
+      case 409: return "Conflict";
+      case 410: return "Gone";
+      case 411: return "Length Required";
+      case 412: return "Precondition Failed";
+      case 413: return "Request Entity Too Large";
+      case 414: return "Request-URI Too Large";
+      case 415: return "Unsupported Media Type";
+      case 416: return "Requested range not satisfiable";
+      case 417: return "Expectation Failed";
+      case 500: return "Internal Server Error";
+      case 501: return "Not Implemented";
+      case 502: return "Bad Gateway";
+      case 503: return "Service Unavailable";
+      case 504: return "Gateway Time-out";
+      case 505: return "HTTP Version not supported";
+      default:  return "Invalid code.";
     }
 }
